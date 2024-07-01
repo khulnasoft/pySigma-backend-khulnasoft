a02f0eeba09b80339e612967246cb439a6aefc18
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-07-02 05:25:16 +0600

    Update print-coverage.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- print-coverage.py
+++ print-coverage.py
@@ -1,7 +1,7 @@
 # Prints code testing coverage as percentage for badge generation.
-from defusedxml.ElementTree import parse
+import xml.etree.ElementTree as et
 
-tree = parse("cov.xml")
+tree = et.parse("cov.xml")
 root = tree.getroot()
 coverage = float(root.attrib["line-rate"]) * 100
 print(f"COVERAGE={coverage:3.4}%")
